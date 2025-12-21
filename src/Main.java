771ff95c3e447c677bef1c15b1f24d443db657eb
OussamaBelkadi <OussamaYC@hotmail.com>
2025-12-21 15:57:24 +0100

    feat(singleton): implement Logger using Singleton pattern

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 import FactoryDesign.Notification;
 import FactoryDesign.NotificationFactory;
 import FactoryDesign.enums.NotificationType;
+import SingletonDesign.Logger;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws Exception {
+        factoryExample();
+    }
+    // Factory Design
+    private static void factoryExample() throws IllegalAccessException{
         Notification email = NotificationFactory.createNotification(NotificationType.Email);
         email.send();
 
         Notification sms = NotificationFactory.createNotification(NotificationType.SMS);
         sms.send();
+    }
+
+    // Singleton Design
+    private static void singletonExample(){
+        Logger logger = Logger.getInstance();
+        logger.log("Log Message");
     }
 }
